cd26f3e0525a5c58d5bad57030f6b49e00488f6e
Niya Neykova <natealizardd@gmail.com>
2025-05-24 19:48:24 +0300

    update guest class

--- Guest.h
+++ Guest.h
@@ -9,25 +9,25 @@ enum guestStatus {
 };
 class Guest {
 private:
-    int id;
+    int id=0;
     char* name=nullptr;
-    int* phone = nullptr;
+    char* phone = nullptr;
     char* email = nullptr;
     guestStatus status;
     void free();
     void copyFrom(const Guest& other);
 public:
-    Guest(int _id,const char* _name, const int* _phone, const char* _email, guestStatus _status);
+    Guest(int _id,const char* _name, const char* _phone, const char* _email, guestStatus _status);
     Guest(const Guest& other);
     Guest& operator=(const Guest& other);
     ~Guest();
     void setName(const char* _name);
-    void setPhone(const int* _phone);
+    void setPhone(const char* _phone);
     void setEmail(const char* _email);
     void setStatus(guestStatus _status);
     [[nodiscard]] int getID() const;
     [[nodiscard]] const char* getName() const;
-    [[nodiscard]] const int* getPhone() const;
+    [[nodiscard]] const char* getPhone() const;
     [[nodiscard]] const char* getEmail() const;
     [[nodiscard]] guestStatus getStatus() const;
     void print() const;
