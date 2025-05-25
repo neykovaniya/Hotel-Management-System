b991841c9497e8b1ac2fbd45e6795373cde094b0
Niya Neykova <natealizardd@gmail.com>
2025-05-25 15:32:10 +0300

    Update main.cpp
    added a test code for the guest class

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "Reservation.h"
 
 const int MAX_ROOMS = 10;
+const int MAX_GUESTS = 10;
 
 int main() {
     Room *rooms[MAX_ROOMS];
@@ -13,15 +14,27 @@ int main() {
     rooms[roomCount++] = new Room(2, DELUXE, RESERVED, 150.0);
     rooms[roomCount++] = new Room(3, APARTMENT, AVAILABLE, 200.0);
 
+    Guest *guests[MAX_GUESTS];
+    int guestCount = 0;
+    guests[guestCount++] = new Guest(1, "Niya", "0888123456", "niya@gmail.com", GOLD);
+    guests[guestCount++] = new Guest(2, "Dari", "0888777666", "dari@gmail.com", REGULAR);
+
     std::cout << "----------rooms----------" << std::endl;
-    for (int i = 0; i < roomCount; ++i) {
+    for (int i = 0; i < roomCount; i++) {
         rooms[i]->print();
         std::cout << "--------------------------" << std::endl;
     }
 
-    for (int i = 0; i < roomCount; ++i) {
+    for (int i = 0; i < roomCount; i++) {
         delete rooms[i];
     }
-
+    std::cout << "----------guests----------" << std::endl;
+    for (int i = 0; i < guestCount; i++) {
+        guests[i]->print();
+        std::cout << "--------------------------" << std::endl;
+    }
+    for (int i = 0; i < guestCount; i++) {
+        delete guests[i];
+    }
     return 0;
 }
